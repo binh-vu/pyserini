f40b6d20b8e82adaf6533969c80240e8754fabf2
Binh Vu <binh@toan2.com>
2023-12-18 22:59:03 -0800

    fix faiss, install faiss-cpu if gpu isn't available

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
+import GPUtil
 import setuptools
 
 with open("project-description.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+if len(GPUtil.getGPUs()) == 0:
+    # change faiss-gpu to faiss-cpu
+    for i in range(len(requirements)):
+        if requirements[i].find("faiss-gpu") != -1:
+            requirements[i] = requirements[i].replace("gpu", "cpu")
+
 pyserini_packages = setuptools.find_packages()
-pyserini_packages.remove('tests')
+pyserini_packages.remove("tests")
 # For some reason, not automatically discovered
-pyserini_packages.append('pyserini.2cr')
+pyserini_packages.append("pyserini.2cr")
 
 setuptools.setup(
     name="pyserini",
@@ -22,14 +29,16 @@ setuptools.setup(
     url="https://github.com/castorini/pyserini",
     install_requires=requirements,
     packages=pyserini_packages,
-    package_data={"pyserini": [
-        "resources/jars/anserini-*-fatjar.jar",
-    ]},
+    package_data={
+        "pyserini": [
+            "resources/jars/anserini-*-fatjar.jar",
+        ]
+    },
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires=">=3.10",
 )
